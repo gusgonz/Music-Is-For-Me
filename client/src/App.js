bb8786ee2d1c44a9883147cd5785bbc4084bf368
Austin Smith <austin.smith.ads@gmail.com>
2020-05-01 13:05:20 -0400

    adding necessary imports for react bootstrap, added react hooks

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./pages/Posts";
+import CreatePost from './pages/CreatePost';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Posts></Posts>
+        {/* <Posts></Posts> */}
+        <CreatePost />
       </div>
     );
   }
