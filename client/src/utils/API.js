e7b5d5eb867ff361549d0cb822a5987afd84863b
Garrett Reichman <gsreichman@gmail.com>
2020-04-30 21:10:09 -0400

    added posts page

---
+++ client/src/utils/API.js
@@ -0,0 +1,48 @@
+const posts = [
+    {
+        "title": "Garrett Reichman & The Funky Bunch",
+        "name": "Vocal Coach",
+        "url": "http://www.youtube.com/watch?v=99j0zLuNhi8",
+        "userID": 69,
+        "email": "gsreichman@gmail.com",
+        "type": "Teacher"
+    },
+    {
+        "title" : "gsr",
+        "name" : "guitar",
+        "url" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
+        "userID" : 1337,
+        "email" : "gsreichman@gmail.com",
+        "type" : "Student"
+    },
+    {
+        "title" : "rsg",
+        "name" : "ukelele, Guitar",
+        "url" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
+        "userID" : 12,
+        "email" : "gsreichman@gmail.com",
+        "type" : "Teacher"
+    },
+    {
+        "title" : "test",
+        "name" : " vocals",
+        "url" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
+        "userID" : 96,
+        "email" : "gsreichman@gmail.com",
+        "type" : "Student"
+    },
+    {
+        "title" : "duke nukem",
+        "name" : "drums",
+        "url" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
+        "userID" : 2,
+        "email" : "gsreichman@gmail.com",
+        "type" : "Teacher"
+    }
+]
+
+export default {
+    getPosts : function(){
+        return posts;
+    }
+}
