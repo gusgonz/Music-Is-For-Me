5f6a5c556756aad8ea73ce39d762ae12e6fe2494
GusGo <gus.gonzalez.15@gmail.com>
2020-05-02 13:28:33 -0400

    user model updates

--- models/user.js
+++ models/user.js
@@ -2,8 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    third_party_auth: [ThirdPartyProviderSchema],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+},
+    { strict: false }
+);
 
-});
+userSchema.virtual('firstname').get(function () { return this.firstName.charAt(0).toUpperCase() + this.firstName.splice(1) });
 
 const User = mongoose.model("User", userSchema);
 
