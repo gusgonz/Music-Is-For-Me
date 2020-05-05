1f7cf77c9ec902e36889b5a8d21375e573493cae
Austin Smith <austin.smith.ads@gmail.com>
2020-05-05 19:48:53 -0400

    added login page and required import into app js file

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import CreatePost from "./pages/CreatePost";
 import ProfilePage from "./pages/ProfilePage";
+import Login from './pages/LoginPage';
 
 function App() {
   const [currUser, setCurrUser] = useState({
