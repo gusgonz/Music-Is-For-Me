463c24d5e5d211de2c4db402832794cd319eae49
Garrett Reichman <gsreichman@gmail.com>
2020-05-07 19:20:48 -0400

    post page is blank until user selects teacher/student

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,62 +1,9 @@
 const axios = require("axios");
 
-const posts = [
-    {
-        "name": "Garrett Reichman & The Funky Bunch",
-        "role": "Vocal Coach",
-        "video": "http://www.youtube.com/watch?v=99j0zLuNhi8",
-        "author_id": 69,
-        "email": "gsreichman@gmail.com",
-        "title": "Teacher",
-        "description" : "sample description",
-        "phone": "867-53-09"
-    },
-    {
-        "name" : "gsr",
-        "role" : "guitar",
-        "video" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
-        "author_id" : 1337,
-        "email" : "gsreichman@gmail.com",
-        "title" : "Student",
-        "description" : "sample description",
-        "phone": "867-53-09"
-    },
-    {
-        "name" : "rsg",
-        "role" : "ukelele, Guitar",
-        "video" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
-        "author_id" : 12,
-        "email" : "gsreichman@gmail.com",
-        "title" : "Teacher",
-        "description" : "sample description",
-        "phone": "867-53-09"
-    },
-    {
-        "name" : "test",
-        "role" : " vocals",
-        "video" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
-        "author_id" : 96,
-        "email" : "gsreichman@gmail.com",
-        "title" : "Student",
-        "description" : "sample description",
-        "phone": "867-53-09"
-    },
-    {
-        "name" : "duke nukem",
-        "role" : "drums",
-        "video" : "http://www.youtube.com/watch?v=99j0zLuNhi8",
-        "author_id" : 2,
-        "email" : "gsreichman@gmail.com",
-        "title" : "Teacher",
-        "description" : "sample description",
-        "phone": "867-53-09"
-    }
-]
 
 export default {
     getPosts: function () {
         return axios.get("/api/posts");
-        // return posts;
     },
     registerLogin: function (user) {
         return axios.post("/auth/register_login", user)
