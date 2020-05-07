d3fb58bd561b617ba628137f5e52c6fca5617e05
Austin Smith <austin.smith.ads@gmail.com>
2020-05-07 16:38:23 -0400

    added styling to text to make more visible

--- client/src/components/DescriptionCreatePost/index.js
+++ client/src/components/DescriptionCreatePost/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import "./style.css";
 
 function DescriptionCreatePost() {
     return(
         <Form.Group controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Description of post</Form.Label>
+        <Form.Label className="DescriptionOfPostText">Description of post</Form.Label>
         <Form.Control as="textarea" rows="3" />
         </Form.Group>
     )
