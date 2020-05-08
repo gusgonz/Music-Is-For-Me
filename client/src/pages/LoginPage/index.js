71c0b5b304785b07d975ad198e2890b35be9b8c8
Garrett Reichman <gsreichman@gmail.com>
2020-05-07 20:38:07 -0400

    added redirect to login page

--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 import Alert from "react-bootstrap/Alert";
 import API from "../../utils/API";
 import UserContext from "../../utils/UserContext";
+import { Redirect } from "react-router-dom";
 
 
 
@@ -13,7 +14,7 @@ function Login() {
   const password = useRef();
   const userState = useContext(UserContext);
 
-  const [loginState, setLoginState] = useState({loginFailed : false});
+  const [loginState, setLoginState] = useState({loginFailed : false, loggedIn : false});
 
   function GetLoginInfo(event) {
     event.preventDefault();
@@ -24,6 +25,7 @@ function Login() {
     console.log(user);
     API.registerLogin(user).then(results => {
       userState.setCurrUser(results.data.user);
+      setLoginState({...loginState, loggedIn: true});
     }).catch(e =>{
       setLoginState({
         loginFailed: true
@@ -31,28 +33,36 @@ function Login() {
     });
   }
 
-  return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label className="DescriptionOfPostText">Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" ref={email} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-    </Form.Text>
-      </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label className="DescriptionOfPostText">Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" ref={password} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={GetLoginInfo}>
-        Submit
-  </Button>
-  <Alert variant="danger" show={loginState.loginFailed} onClose={() => setLoginState({loginFailed: false})} dismissible>
-    The email and password you entered do not match what is in our system. Please try again.
-  </Alert>
-    </Form>
-  )
-}
+  if(loginState.loggedIn){
+    return <Redirect to="/posts"></Redirect>
+  }
+  else{
+
+    return (
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="DescriptionOfPostText">Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" ref={email} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+        </Form.Text>
+          </Form.Group>
+      
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label className="DescriptionOfPostText">Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" ref={password} />
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={GetLoginInfo}>
+            Submit
+      </Button>
+      <Alert variant="danger" show={loginState.loginFailed} onClose={() => setLoginState({loginFailed: false})} dismissible>
+        The email and password you entered do not match what is in our system. Please try again.
+      </Alert>
+        </Form>
+      
+      )
+    }
+    }
 
 export default Login;
--- client/src/pages/Posts/index.js
+++ client/src/pages/Posts/index.js
@@ -19,7 +19,7 @@ function Posts() {
 
     useEffect(() => {
         loadPosts();
-        console.log(userState.currUser)
+        console.log(userState)
     },[]);
 
     function loadPosts(){
