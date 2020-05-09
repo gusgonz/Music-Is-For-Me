446011b76ca4d22ea8b2a86056ca90387abc93d3
GusGo <gus.gonzalez.15@gmail.com>
2020-05-09 13:49:01 -0400

    framework for update and delete post by id

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,5 +14,19 @@ module.exports = {
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+    },
+    delete: function (req, res) {
+        db.Post
+            .findByIdAndDelete(req.params.id)
+            .then(result => res.json(result))
+            .catch(err => res.json(err));
+    },
+    edit: function (req, res) {
+        db.Post
+            .findById(req.params.id)
+            .then(post => {
+
+            })
+            .catch(err => res.json(err));
     }
 }
--- routes/api/index.js
+++ routes/api/index.js
@@ -7,6 +7,10 @@ router.route("/posts")
     .get(postController.findAll)
     .post(postController.create);
 
+router.route("/posts/:id")
+    .put(postController.edit)
+    .delete(postController.delete)
+
 // route: "/api/users/:id"
 router.route("/users/:id")
     .get(userController.findByID)
