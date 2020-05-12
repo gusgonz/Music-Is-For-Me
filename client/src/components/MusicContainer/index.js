f6ebf8a3d37503720d21dba323fcb3d6d7fc7972
Garrett Reichman <gsreichman@gmail.com>
2020-05-12 19:28:49 -0400

    posts page initially loads all posts, added inline style to own posts

--- client/src/components/MusicContainer/index.js
+++ client/src/components/MusicContainer/index.js
@@ -11,13 +11,24 @@ import "./style.css"
 
 function MusicContainer(props) {
 console.log(props.author_id);
+let renderStyle={};
+if(props.ownPost){
+ renderStyle= {
+    border: "2px solid blue",
+    backgroundColor: "gray",
+    color:"black"
+  }
+}
+else{
+renderStyle={
+  border: "2px solid blue",
+  backgroundColor: "white",
+  color:"black"
+}
+}
   return (
 
-    <Media  style={{
-      border: "2px solid blue",
-      backgroundColor: "white",
-      color:"black"
-    }}>
+    <Media  style={renderStyle}>
       <Media.Body>
         <Row>
           <Col>
@@ -25,9 +36,11 @@ console.log(props.author_id);
             <h4>Role: {props.role}</h4>
             <span>Email:</span><p>{props.email}</p>
             <span>Phone:</span><p>{props.phone}</p>
-            <p>{props.title} #remove later#</p>
             <p>{props.description}</p>
-            {props.ownPost? <p>hi</p> : ""}
+            {props.ownPost ? 
+            <p>hi</p> 
+            : ""
+            }
           </Col>
           <Col lg={5} md={5} sm={5} xs={5}>
             <MusicPlayer
