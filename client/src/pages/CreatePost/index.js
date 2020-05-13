f39e5c03818c99f694489a0e10d08b61cbfbb464
Garrett Reichman <gsreichman@gmail.com>
2020-05-12 21:02:57 -0400

    redirects to post page when post is created

--- client/src/pages/CreatePost/index.js
+++ client/src/pages/CreatePost/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
+import { Redirect } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import InputGroup from 'react-bootstrap/InputGroup';
 import Dropdown from "react-bootstrap/Dropdown";
@@ -15,139 +16,110 @@ import API from "../../utils/API";
 
 
 function CreatePost() {
-    // const [TeacherOrStudent, SetStateOfTeacherOrStudent] = useState("");
-    // const [Output, SetOutput] = useState({});
-    // const [Instrument, SetInstrument] = useState("");
-    // const [FormState, SetFormState] = useState("");
-    
-    
-    // Enable below line to enable user context along with importing
-    const userState = useContext(UserContext);
-    // const [Instruments, SetInstruments] = useState([]);
-    // const [UserDescription, SetUserDescription] = useState("");
-    // const [UserDescription, SetUserDescription] = useState("");
-// email, username, userID from global context
-    // const TeacherOrStudent = useRef();
 
-    const teacherOrStudent = useRef();
-    const instrumentPicked = useRef();
-    const description = useRef();
-    const urlYoutube = useRef();
-    const phoneNumber = useRef();
+  const userState = useContext(UserContext);
+  const teacherOrStudent = useRef();
+  const instrumentPicked = useRef();
+  const description = useRef();
+  const urlYoutube = useRef();
+  const phoneNumber = useRef();
 
-    // const Description = useRef();
+  const [postCreated, setPostCreated] = useState(false);
+
+  // const Description = useRef();
 
 
-    // Use below for getting info using useRef based on what user put in, this is boilerplate from loginpage
-    // function GetCreatePostInfo(event) {
-    //     event.preventDefault();
-    //     SetInstrument(instrumentRef);
-        function createPostInfo() {
-            // console.log(teacherOrStudent.current.value);
-            // console.log(instrumentPicked.current.value);
-            // console.log(description.current.value);
-            // console.log(urlYoutube.current.value);
+  // Use below for getting info using useRef based on what user put in, this is boilerplate from loginpage
+  // function GetCreatePostInfo(event) {
+  //     event.preventDefault();
+  //     SetInstrument(instrumentRef);
+  function createPostInfo() {
+    // console.log(teacherOrStudent.current.value);
+    // console.log(instrumentPicked.current.value);
+    // console.log(description.current.value);
+    // console.log(urlYoutube.current.value);
 
-            //below creates object of all the user inputs
-            let CreatePostObject = {
-                title: teacherOrStudent.current.value,
-                role: instrumentPicked.current.value,
-                description: description.current.value,
-                video: urlYoutube.current.value,
-                author_id: userState.currUser._id,
-                email: userState.currUser.email,
-                phone: phoneNumber.current.value,
-                name:"temp name"
-            }
-            console.log(CreatePostObject);
-            API.createPost(CreatePostObject).then(results =>{
-              console.log(results);
-            })
+    //below creates object of all the user inputs
+    let CreatePostObject = {
+      title: teacherOrStudent.current.value,
+      role: instrumentPicked.current.value,
+      description: description.current.value,
+      video: urlYoutube.current.value,
+      author_id: userState.currUser._id,
+      email: userState.currUser.email,
+      phone: phoneNumber.current.value,
+      name: "temp name"
+    }
+    console.log(CreatePostObject);
+    API.createPost(CreatePostObject).then(results => {
+      console.log(results);
+      setPostCreated(true);
+    })
 
-            }
-            // SetStateOfTeacherOrStudent(Instrument);
-            // Instrument: instrumentPicked.current.value,
-            // description: description.current.value,
-            // teacherOrStudent: teacherOrStudent.current.value
-            
+  }
 
-        
-        // console.log(instrumentPicked.current.current.innerText);
-        // console.log(Instrument);
-        // API.registerLogin(user);
-    
-    
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
 
-    // Similar to componentDidMount and componentDidUpdate:
-    useEffect(() => {
-        // document.title = `You clicked ${count} times`;
-      });
+  });
 
-      if (!userState.currUser._id){
-        return  <Container>
-        <h1>You Must Be Logged In To View This Page</h1>
-        </Container>
-      }
+  if (!userState.currUser._id) {
+    return <Container>
+      <h1>You Must Be Logged In To View This Page</h1>
+    </Container>
+  }
+
+  else if (postCreated) {
+    return <Redirect to="/posts"></Redirect>
+  }
 
 
-    return(
-      <Container>
-        <div>
-              <>
-              <Form.Control as="select" ref={teacherOrStudent}>
-                        <option>Teacher</option>
-                        <option>Student</option>
-                    </Form.Control>
+  return (
+    <Container>
+      <div>
+        <>
+          <Form.Control as="select" ref={teacherOrStudent}>
+            <option>Teacher</option>
+            <option>Student</option>
+          </Form.Control>
 
-  <InputGroup>
-    <FormControl
-      ref={instrumentPicked}
-      placeholder="What instrument?"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-</InputGroup>
-<InputGroup>
-    <FormControl
-      ref={description}
-      placeholder="Description of post"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-  </InputGroup>
+          <InputGroup>
+            <FormControl
+              ref={instrumentPicked}
+              placeholder="What instrument?"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+          </InputGroup>
+          <InputGroup>
+            <FormControl
+              ref={description}
+              placeholder="Description of post"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+          </InputGroup>
 
-  <InputGroup>
-    <FormControl
-      ref={urlYoutube}
-      placeholder="URL of example youtube"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-  </InputGroup>
-  <InputGroup>
-    <FormControl
-      ref={phoneNumber}
-      placeholder="Phone number: optional"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-  </InputGroup>
-  <Button  type="submit"  onClick={createPostInfo}>Submit</Button>{' '}
-</>
-        {/* <DropDownMenu instrumentRef={SetInstrument}/> */}
-        {/* <TeacherStudentButtons TeacherOrStudent = {SetStateOfTeacherOrStudent}/> */}
-        {/* <TeacherStudentButtons TeacherOrStudent = {SetStateOfTeacherOrStudent}/>
-        <DescriptionCreatePost />
-        <ExampleSongCreatePost />
-        <SubmitButtonCreatePost GetCreatePostInfo={GetCreatePostInfo} /> */}
-        {/* <Button
-Button className="submitButton" variant="primary" onClick={() => setCount(count + 1)}>
-            Submit
-        </Button>{' '} */}
-        {/* <p className="DescriptionOfPostText">{TeacherOrStudent}</p> */}
-        </div>
-        </Container>
-    )
+          <InputGroup>
+            <FormControl
+              ref={urlYoutube}
+              placeholder="URL of example youtube"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+          </InputGroup>
+          <InputGroup>
+            <FormControl
+              ref={phoneNumber}
+              placeholder="Phone number: optional"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+          </InputGroup>
+          <Button type="submit" onClick={createPostInfo}>Submit</Button>{' '}</>
+      </div>
+    </Container>
+  )
 }
 
 export default CreatePost;
