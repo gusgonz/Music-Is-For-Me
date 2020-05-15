9f88a124f7a8898af267c9d6c9b23d9f13836c4f
Austin Smith <austin.smith.ads@gmail.com>
2020-05-15 16:59:55 -0400

    got twitter facebook logo to align right

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,16 +6,19 @@ import './style.css'
 function Footer() {
     return (
         <Navbar expand="xl" fixed="bottom" className="blackFooter">
-
-            <img src={require('../../assets/icons/redSendIcon.png')} alt="Send email arrow" />
+        <div>
             <FormControl
                 placeholder="Email us with any suggestions."
                 aria-label="Email us with any suggestions."
                 aria-describedby="basic-addon2"
                 className="widthFormControlInFooter"
             />
-            <img src={require('../../assets/icons/twitter.svg')} alt="Twitter Icon" width='40px' height='40px' />
+            <img src={require('../../assets/icons/redSendIcon.png')} alt="Send email arrow" />
+        </div>
+        <div className="ml-md-auto">
+            <img src={require('../../assets/icons/twitter.svg')} alt="Twitter Icon" width='25px' height='25px' />
             <img src={require('../../assets/icons/facebook.svg')} alt="Facebook Icon" />
+        </div>
         </Navbar >
     )
 }
