5179814df9f4da2b92a4d1b290fe2b94890422cc
Austin Smith <austin.smith.ads@gmail.com>
2020-05-15 17:33:26 -0400

    twitter facebook logo are a tags now without links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -16,8 +16,8 @@ function Footer() {
             <a href="mailto:someone@yoursite.com"><img className="redSendArrowIcon" src={require('../../assets/icons/redSendIcon.png')} alt="Send email arrow" /></a>
         </div>
         <div className="ml-md-auto">
-            <img src={require('../../assets/icons/twitter.svg')} alt="Twitter Icon" width='25px' height='25px' />
-            <img className="twitterFacebookMargins" src={require('../../assets/icons/facebook.svg')} alt="Facebook Icon" />
+            <a href=""><img src={require('../../assets/icons/twitter.svg')} alt="Twitter Icon" width='25px' height='25px' /></a>
+            <a href=""><img className="twitterFacebookMargins" src={require('../../assets/icons/facebook.svg')} alt="Facebook Icon" /></a>
         </div>
         </Navbar >
     )
